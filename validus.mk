5f96b3ece57e7a426587e4987607d897c8bc4b7f
John Brewer <gzroms@hotmail.com>
2014-11-11 15:03:29 -0500

    Update and rename du.mk to validus.mk

--- validus.mk
+++ validus.mk
@@ -27,14 +27,14 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from our custom product configuration
-$(call inherit-product, vendor/du/config/common.mk)
+$(call inherit-product, vendor/du/config/common_full_phone.mk)
 
 # Inherit from moto_msm8960dt device
 $(call inherit-product, device/motorola/moto_msm8960dt/device_moto_msm8960dt.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := moto_msm8960dt
-PRODUCT_NAME := du_moto_msm8960dt
+PRODUCT_NAME := validus_moto_msm8960dt
 PRODUCT_BRAND := motorola
 PRODUCT_MODEL := MOTOROLA MSM8960DT
 PRODUCT_MANUFACTURER := motorola
