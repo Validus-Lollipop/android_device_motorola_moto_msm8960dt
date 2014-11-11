3ed5dce61067c70ed98a31ccf2dba2424833fcc8
John Brewer <gzroms@hotmail.com>
2014-11-11 15:36:58 -0500

    derp

--- validus.mk
+++ validus.mk
@@ -27,7 +27,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from our custom product configuration
-$(call inherit-product, vendor/du/config/common_full_phone.mk)
+$(call inherit-product, vendor/validus/config/common_full_phone.mk)
 
 # Inherit from moto_msm8960dt device
 $(call inherit-product, device/motorola/moto_msm8960dt/device_moto_msm8960dt.mk)
